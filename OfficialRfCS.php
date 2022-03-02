10e682dd198ae99f472be0d845eaecc60f425354
chikalet <93155754+chikalet@users.noreply.github.com>
2022-03-02 18:31:07 +0300

    about/

---
+++ OfficialRfCS.php
@@ -0,0 +1,18 @@
+<?php 
+	$title = 'Official replies from Crypto Soul';
+	include('../header.php');
+?>
+	<p class="h">Official replies from Crypto Soul</p>
+<p>Последнее обновление: 15 января 2022 года</p>
+<p>Sometimes, Crypto Soul receives information requests about his clients from law enforcement agencies around the world. This page describes how to deal with law enforcement requests.</p>
+ 
+<p>As a rule, law enforcement agencies are interested in two types of data: information about the personal data of our customers and information about the trading activities of our customers.</p>
+ 
+<p>Crypto Soul provides the requested information only on the basis of an official legal request drawn up in accordance with all requirements of international law or Hong Kong law. Crypto Soul provides only the information specified in the request and complies with international law or Hong Kong law.</p>
+ 
+<p>Crypto Soul also reserves the right to disclose information to authorities, law enforcement agencies in order to protect their own interests, as well as the interests of their customers.</p>
+
+
+<?php 
+	include('../footer.html');
+?>
